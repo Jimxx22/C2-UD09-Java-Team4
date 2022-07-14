83bc6afe787d533b22ccc39df1c5cb8959313bc8
David Marcos <david.13254@gmail.com>
2022-07-14 13:17:03 +0200

    main

--- UD09-03/src/MainApp.java
+++ UD09-03/src/MainApp.java
@@ -2,7 +2,16 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Libro libro1 = new Libro("978-84-08-25733-2","La ladrona de huesos","Manel Loureiro",504);
+		Libro libro2 = new Libro("978-84-233-6158-8","El camino del fuego","Maria Oruna",400);
+		
+		System.out.println(libro1);
+		System.out.println(libro2);
+		if(libro1.getPaginas()>libro2.getPaginas) {
+			System.out.println("El libro con max paginas es "+libro1.getPaginas());
+		} else {
+			System.out.println("El libro con max paginas es "+libro2.getPaginas());
+		}
 
 	}
 
