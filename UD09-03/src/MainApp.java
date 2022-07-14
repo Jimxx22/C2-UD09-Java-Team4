9d69ec4640629fedcbf7d1f2a46c288bee8eab77
Jimxx22 <jaumegil22@gmail.com>
2022-07-14 13:46:24 +0200

    aa

--- UD09-03/src/MainApp.java
+++ UD09-03/src/MainApp.java
@@ -3,7 +3,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 
-		/*
+		
 		Libro libro1 = new Libro("978-84-08-25733-2","La ladrona de huesos","Manel Loureiro",504);
 		Libro libro2 = new Libro("978-84-233-6158-8","El camino del fuego","Maria Oruna",400);
 		
@@ -14,7 +14,7 @@ public class MainApp {
 		} else {
 			System.out.println("El libro con max paginas es "+libro2.getPaginas());
 		}
-		*/
+		
 
 	}
 
