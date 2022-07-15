1bdb58b4c460be4cfae23e814f23ab9ba0eff31f
David Marcos <david.13254@gmail.com>
2022-07-15 10:21:10 +0200

    calcular

--- UD09-04/src/Raices.java
+++ UD09-04/src/Raices.java
@@ -9,6 +9,20 @@ public class Raices {
 		this.c = c;
 	}
 	
+	public boolean tieneRaiz() {
+		if(getDiscriminante() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public void calcular() {
+		if(tieneRaiz()) {
+			
+		} else if (tieneRaices()) {
+			
+		}
+	}
 
 }
