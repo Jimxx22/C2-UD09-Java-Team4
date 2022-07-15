7ac66e223c140de3dc4910fa33671821275a5bbe
David Marcos <david.13254@gmail.com>
2022-07-15 09:11:40 +0200

    declarar metodos main

--- UD09-06/src/MainApp.java
+++ UD09-06/src/MainApp.java
@@ -1,9 +1,34 @@
 
 public class MainApp {
+	
+	final static int NUM_ESPECTADORES = 30;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Cine cine = new Cine();
+		 
+		for (int i = 0; i < NUM_ESPECTADORES; i++) {
+			String nombre;
+			int edad;
+			double dinero;
+			Espectador espectador = new Espectador();
+		}
 
+	}
+	
+	private static String generarNombre() {
+		
+		return "";
+	}
+	
+	private static int generarEdad() {
+		
+		return 0;
+	}
+	
+	private static double generarDinero() {
+		
+		return 0.0;
 	}
 
 }
