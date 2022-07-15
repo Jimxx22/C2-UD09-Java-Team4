d654e65bb5c6eef71b1b9e27737ee3d0b8d18a65
Jimxx22 <jaumegil22@gmail.com>
2022-07-15 11:17:16 +0200

    Persona

---
+++ UD09-5/src/Persona.java
@@ -0,0 +1,53 @@
+
+public abstract class Persona {
+
+	protected String nombre;
+	protected int edad;
+	protected char sexo;
+	protected boolean asistencia;
+	
+	
+	
+	public Persona(String nombre, int edad, char sexo, boolean asistencia) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.asistencia = asistencia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public boolean isAsistencia() {
+		return asistencia;
+	}
+
+	public void setAsistencia(boolean asistencia) {
+		this.asistencia = asistencia;
+	}
+	
+	public abstract boolean novillos();
+	
+}
