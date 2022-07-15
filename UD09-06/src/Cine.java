12c93df37949059a40a15f090646828c32c3bd50
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-15 09:24:13 +0200

    Matriz de asientos

--- UD09-06/src/Cine.java
+++ UD09-06/src/Cine.java
@@ -3,15 +3,26 @@ public class Cine {
 	
 	private Pelicula peli;
 	private double precio;
+	private Asiento[][] asientos;
 	
 	public Cine() {
+		this.peli = new Pelicula();
 		this.precio = 0.0;
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 9 ; j++) {
+				this.asientos [i][j] = new Asiento(i,(char)(64 + j));
+			}
+		}
 	}
 
 	public Cine(Pelicula peli, double precio) {
-		super();
 		this.peli = peli;
 		this.precio = precio;
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 9 ; j++) {
+				this.asientos [i][j] = new Asiento(i,(char)(64 + j));
+			}
+		}
 	}
 
 	public Pelicula getPeli() {
@@ -29,11 +40,22 @@ public class Cine {
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+	
+	public boolean compareTo (Object a) {
+		Espectador e1 = (Espectador) a;
+		if (e1.getDinero() >= precio && e1.getEdad() >= peli.getEdadMinima()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 	@Override
 	public String toString() {
-		return "Cine [precio=" + precio + "]";
+		return "Cine [peli=" + peli + ", precio=" + precio + "]";
 	}
+
+	
 	
 	
 }
