1b91d382cbaf4d25b31a0458c0e25b4d4c2f3414
David Marcos <david.13254@gmail.com>
2022-07-15 09:25:13 +0200

    metodos aleatorios

--- UD09-06/src/MainApp.java
+++ UD09-06/src/MainApp.java
@@ -17,17 +17,47 @@ public class MainApp {
 	}
 	
 	private static String generarNombre() {
+		String nombre = "";
+		int longitudNombre, vocal;
+		longitudNombre = (int)Math.random()*6+3;		//Longitud del nombre aleatoria de 3 a 9
+		
+		for(int i = 0; i < longitudNombre; i++) {
+			if(i%2==0) {
+				nombre += (char)Math.random()*26+97;	//Generar letra aleatoria
+			} else {
+				vocal = (int) (Math.random()*5);		//generar letra vocal aleatoria
+				switch (vocal) {
+				case 0:
+					nombre += 'a';
+					break;
+				case 1:
+					nombre += 'i';
+					break;
+				case 2:
+					nombre += 'u';
+					break;
+				case 3:
+					nombre += 'e';
+					break;
+				case 4:
+					nombre += 'o';
+					break;
+				}
+			}
+		}
 		
 		return "";
 	}
 	
 	private static int generarEdad() {
-		
+		int edad;
+		edad = (int)Math.random()*100;
 		return 0;
 	}
 	
 	private static double generarDinero() {
-		
+		double dinero;
+		dinero = Math.random()*50;
 		return 0.0;
 	}
 
