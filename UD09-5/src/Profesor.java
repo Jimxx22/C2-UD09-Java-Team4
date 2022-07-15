ff19fdd5bf159989c8746acf168d7dd99aec97d1
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-15 11:34:20 +0200

    Clase profesor

---
+++ UD09-5/src/Profesor.java
@@ -0,0 +1,44 @@
+
+public class Profesor extends Persona{
+	private String materia;
+	
+	
+	public Profesor() {
+		super();
+		this.materia = "";
+	}
+
+	public Profesor(String nombre, int edad, char sexo, boolean asistencia, String materia) {
+		super(nombre, edad, sexo, true);
+		if (validarMateria(materia)) {
+			this.materia = materia;
+		}else {
+			this.materia = "";
+		}
+		novillos();
+	}
+
+
+
+	private boolean validarMateria (String materia) {
+		String [] materias = {"matematicas","filosofia","fisica"};
+		int i = 0;
+		boolean valido = false;
+		while (i < materias.length && !valido) {
+			if (materias[i].equals(materia.toLowerCase())) {
+				valido =true;
+			}
+			i++;
+		}
+		return valido;
+	}
+
+	@Override
+	public void novillos() {
+		// TODO Auto-generated method stub
+		int probabilidad = (int)(Math.random()*5+1);
+		if (probabilidad == 1) {
+			asistencia = false;
+		}
+	}
+}
