073c2cec8ea3baef98b731a5439f10e92ab7b7d7
Jimxx22 <jaumegil22@gmail.com>
2022-07-17 20:22:50 +0200

    Comentarios y modificación del main

--- UD09-1/src/Lavadora.java
+++ UD09-1/src/Lavadora.java
@@ -1,34 +1,41 @@
 
 public class Lavadora extends Electrodomestico{
+	// La clase lavadora hereda de la clase electrodoméstico
 	
+	// Variables
 	private Double carga;
+	
+	// Constantes
 	private final static Double CARGA_DEF=5.0;
 
+	// Constructor por defecto usando la constante
 	public Lavadora() {
 		super();
 		this.carga = CARGA_DEF;
 	}
 	
-
+	// Constructor que pide el precio y el peso
 	public Lavadora(double precio, double peso) {
 		super(precio, peso);
-		// TODO Auto-generated constructor stub
+		this.carga = CARGA_DEF;
 	}
 
-
+	// Constructor con todos los parámetros
 	public Lavadora(double precio, String color, char consumo, double peso, double carga) {
 		super(precio, color, consumo, peso);
 		this.carga=carga;
 	}
 
+	// Geter
 	public Double getCarga() {
 		return carga;
 	}
 
-	
+	// Método precioFinal que amplía el método de la clase electrodoméstico
 	public double precioFinal() {
-		super.precioFinal();
+		precio = super.precioFinal();
 		
+		// Si la carga es mayor de 30 suma 50 al precio
 		if(carga>30) {
 			precio+=50;
 		}
