c667a16e189e4459cd0e5b8f044972454311089a
Jimxx22 <jaumegil22@gmail.com>
2022-07-17 19:52:46 +0200

    Comentarios y modificación del main

--- UD09-02/src/Videojuego.java
+++ UD09-02/src/Videojuego.java
@@ -1,20 +1,25 @@
 
 public class Videojuego implements Entregable{
-
+	// Clase videojuego que implementa la interfaz entregable
+	
+	// Variables
 	private String titulo;
 	private int horasEstimadas;
 	private boolean entregado;
 	private String genero;
 	private String compania;	
 	
+	// Cosntructor por defecto
 	public Videojuego() {
 		this("",10,false,"","");
 	}
 	
+	// Constructo que pide el título y las horas estimadas
 	public Videojuego(String titulo, int horasEstimadas) {
 		this(titulo,horasEstimadas,false,"","");
 	}
 	
+	// Constructor que pide todos los parámetros
 	public Videojuego(String titulo, int horasEstimadas, boolean entregado, String genero, String compania) {
 		this.titulo = titulo;
 		this.horasEstimadas = horasEstimadas;
@@ -23,6 +28,7 @@ public class Videojuego implements Entregable{
 		this.compania = compania;
 	}
 
+	// Geters y seters
 	public String getTitulo() {
 		return titulo;
 	}
@@ -55,30 +61,35 @@ public class Videojuego implements Entregable{
 		this.compania = compania;
 	}
 
+	// Método toString
 	@Override
 	public String toString() {
 		return "Videojuego [titulo=" + titulo + ", horasEstimadas=" + horasEstimadas + ", entregado=" + entregado
 				+ ", genero=" + genero + ", compania=" + compania + "]";
 	}
 
+	// Método implementado mediante la interfaz que cambia el estado de entregado a true
 	@Override
 	public void entregar() {
 		this.entregado=true;
 		
 	}
 
+	// Método implementado mediante la interfaz que cambia el estado de entregado a false
 	@Override
 	public void devolver() {
 		this.entregado=false;
 		
 	}
 
+	// Método implementado mediante la interfaz para comprobar el estado de entregado
 	@Override
 	public boolean isEntregado() {
 		
 		return entregado;
 	}
 
+	// Método implementado mediante la interfaz que compara la cantidad de horas estimadas de 2 videojuegos
 	@Override
 	public int compareTo(Object a) {
 		
